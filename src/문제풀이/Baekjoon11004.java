e85e7fddfde60399c337acb33d591bf298ddeb2e
leeyoungchun <dldydcns123@gmail.com>
2022-07-23 12:58:55 +0900

    백준 11004문제 - quick select 알고리즘 수정

--- src/문제풀이/Baekjoon11004.java
+++ src/문제풀이/Baekjoon11004.java
@@ -5,111 +5,65 @@ import java.util.StringTokenizer;
 
 public class Baekjoon11004 {
     public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-//        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-//        int N = Integer.parseInt(st.nextToken());
-        int N = 5;
-//        int K = Integer.parseInt(st.nextToken());
-        int K = 2;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+//        int N = 9;
+        int K = Integer.parseInt(st.nextToken());
+//        int K = 5;
         int[] test = new int[N];
 
-//        st = new StringTokenizer(br.readLine(), " ");
-//        for(int i = 0; i < N; i++){
-//            test[i] = (Integer.parseInt(st.nextToken()));
-//        }
-        test[0] = 4;
-        test[1] = 1;
-        test[2] = 2;
-        test[3] = 3;
-        test[4] = 5;
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < N; i++){
+            test[i] = (Integer.parseInt(st.nextToken()));
+        }
+//        test[0] = 4;
+//        test[1] = 1;
+//        test[2] = 2;
+//        test[3] = 3;
+//        test[4] = 5;
+//        test[5] = 8;
+//        test[6] = 7;
+//        test[7] = 6;
+//        test[8] = 9;
         int result = quickSelect(test, K-1, 0, test.length-1);
-        System.out.println(result);
-//        bw.write(index);
+//        System.out.println(result);
+        bw.write(result);
     }
-
-//    private static List<Integer> sort(List<Integer> test) {
-//        if (test.size() <= 1) {
-//            return test;
-//        }
-//
-//        List<Integer> left;
-//        List<Integer> right;
-//        int mid = (test.size() - 1) / 2;
-//
-//        List<List<Integer>> array = new ArrayList<>(test.stream()
-//                .collect(Collectors.partitioningBy(s -> test.indexOf(s) > mid))
-//                .values());
-//        left = sort(array.get(0));
-//        right = sort(array.get(1));
-//
-//        return merge(left, right);
-//    }
-//
-//    private static List<Integer> merge(List<Integer> left, List<Integer> right) {
-//        List<Integer> result = new ArrayList<>();
-//        int i = 0;
-//        int j = 0;
-//
-//        while(i < left.size() && j < right.size()){
-//            if(left.get(i) < right.get(j)){
-//                result.add(left.get(i));
-//                i++;
-//            }else{
-//                result.add(right.get(j));
-//                j++;
-//            }
-//        }
-//
-//        if(i == left.size()){
-//            while(j < right.size()){
-//                result.add(right.get(j));
-//                j++;
-//            }
-//        }else{
-//            while(i < left.size()){
-//                result.add(left.get(i));
-//                i++;
-//            }
-//        }
-//        return result;
-//    }
-
 
     public static int quickSelect(int[] arr, int key, int left, int right){
         if(right - left <= 0){
             return arr[key];
         }
-        int pivotIndex = partition(arr, left, right);
-        if(key < pivotIndex){
-            return quickSelect(arr, key, left, pivotIndex -1);
-        }else if(key > pivotIndex){
-            return quickSelect(arr, key, pivotIndex + 1, right);
+        int sortedIndex = partition(arr, left, right);
+        if (sortedIndex < key) {
+            return partition(arr, sortedIndex + 1, right);
+        } else if (sortedIndex > key) {
+            return partition(arr, left, sortedIndex - 1);
         }else{
-            return arr[pivotIndex];
+            return arr[sortedIndex];
         }
     }
 
     private static int partition(int[] arr, int left, int right) {
+        int pivotVal = arr[right];
         int pivotIndex = right;
-        int pivot = arr[pivotIndex]; // 오른쪽
         right--;
+
         while(true){
-            while(arr[left] < pivot){
+            while(arr[left] < pivotVal){
                 left++;
             }
-            while(arr[right] > pivot){
+            while(arr[right] > pivotVal){
                 right--;
-                if(right <= 0){
-                    break;
-                }
             }
-            if(left >= right){
+            if(right <= left){
                 break;
-            }else{
-                swap(arr, left, right);
             }
+            swap(arr, left, right);
         }
+
         swap(arr, left, pivotIndex);
         return left;
     }
@@ -119,45 +73,4 @@ public class Baekjoon11004 {
         arr[left] = arr[right];
         arr[right] = temp;
     }
-
-//    function quickselect(arr, kth, left, right) {
-//        if (right - left <= 0) {
-//            return arr[left];
-//        }
-//  const pivotIndex = partition(arr, left, right);
-//        if (kth < pivotIndex) {
-//            return quickselect(arr, kth, left, pivotIndex - 1);
-//        } else if (kth > pivotIndex) {
-//            return quickselect(arr, kth, pivotIndex + 1, right);
-//        } else {
-//            return arr[pivotIndex];
-//        }
-//    }
-//    function partition(arr, left, right) {
-//  const pivotIndex = right;
-//  const pivot = arr[pivotIndex];
-//        right -= 1;
-//        while (true) {
-//            while (arr[left] < pivot) {
-//                left += 1;
-//            }
-//            while (arr[right] > pivot) {
-//                right -= 1;
-//            }
-//            if (left >= right) {
-//                break;
-//            } else {
-//                swap(arr, left, right);
-//            }
-//        }
-//        swap(arr, left, pivotIndex);
-//        return left;
-//    }
-//    // 순서를 바꾸는 함수
-//    function swap(arr, left, right) {
-//  const temp = arr[left];
-//        arr[left] = arr[right];
-//        arr[right] = temp;
-//    }
-
 }
